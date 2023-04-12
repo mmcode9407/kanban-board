cfc674dc37838f6b0674fd606517cb4d08381033
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-04-12 14:12:36 +0200

    configure environment

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+// importuję bibliotękę [path] z [node.js]
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// importuję odpowiedni plugin
+const ESLintPlugin = require('eslint-webpack-plugin'); // importuję odpowiedni plugin
+
+module.exports = {
+    entry: './src/app.js',
+    // definiuje plik wejściowy
+    mode: 'development',
+    // tryb pracy
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        // definiuje ścieżką wyjściową
+        filename: 'app.min.js',
+        // definiuję nazwę pliku wyjściowego
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                // określam jakie pliki
+                // będą brane pod uwagę
+                exclude: /node_modules/,
+                // określam wykluczenia
+                use: 'babel-loader',
+                // określam jaki [loader]
+                // ma być wykorzystany
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+        // obecnie brak dodatkowych ustawień
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            // wskazuje plik źródłowy
+            filename: 'index.html',
+            // określan nazwę dla pliku
+        }),
+        new ESLintPlugin(),
+    ],
+};
+// eksportuję ustawienia dla webpack-a
