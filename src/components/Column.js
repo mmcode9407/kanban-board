7726f8525573beb5588aa0d9ce0d04d470713815
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-04-14 17:18:19 +0200

    create `Task` component and changes in `Column` component

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,42 +1,17 @@
 ﻿import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faForward, faBackward, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+import Task from './Task';
 import BoardContext from '../context/context';
 
 const Column = (props) => {
-    const { tasks, setTasks, columns } = useContext(BoardContext);
+    const { tasks } = useContext(BoardContext);
     const { name, limit, id } = props;
 
-    const couldTaskMove = (value, item) => {
-        const newColumn = columns.find((column) => column.id === item.idColumn + value);
-        if (newColumn) {
-            const tasksInColumn = tasks.filter((task) => task.idColumn === newColumn.id);
-            if (tasksInColumn.length < newColumn.limit) {
-                return true;
-            }
-        }
-        return false;
-    };
+    const renderTasks = () => {
+        const columnTasks = tasks.filter((task) => task.idColumn === id);
 
-    const moveTask = (value, item) => {
-        if (couldTaskMove(value, item)) {
-            const movedTask = tasks.findIndex((task) => task.id === item.id);
-
-            const newState = tasks.map((task, index) => {
-                if (index === movedTask) {
-                    return { ...task, idColumn: task.idColumn + value };
-                }
-                return task;
-            });
-
-            setTasks(newState);
-        }
-    };
-
-    const removeTask = (taskId) => {
-        const tasksAfterRemove = tasks.filter((task) => task.id !== taskId);
-        setTasks(tasksAfterRemove);
+        return columnTasks.map((item) => <Task key={item.id} data={item} />);
     };
 
     return (
@@ -45,39 +20,7 @@ const Column = (props) => {
                 <h2 className="column__header-title">{name}</h2>
                 <p className="column__header-limit"> | {limit}</p>
             </div>
-            <ul className="column__list">
-                {tasks
-                    .filter((task) => task.idColumn === id)
-                    .map((item) => (
-                        <li className="card" key={item.id}>
-                            <h3 className="card__title">{item.taskName}</h3>
-                            <p className="card__description">{item.taskDescription}</p>
-                            <p className="card__owner">{item.taskOwner}</p>
-                            <p className="card__deadline">{item.taskDeadline}</p>
-                            <div className="card__buttons-box">
-                                <button
-                                    className="card__button"
-                                    type="button"
-                                    disabled={!couldTaskMove(-1, item)}
-                                    onClick={() => moveTask(-1, item)}
-                                >
-                                    <FontAwesomeIcon icon={faBackward} className="card__button-icon" />
-                                </button>
-                                <button className="card__button" type="button" onClick={() => removeTask(item.id)}>
-                                    <FontAwesomeIcon icon={faTrash} className="card__button-icon" />
-                                </button>
-                                <button
-                                    className="card__button"
-                                    type="button"
-                                    disabled={!couldTaskMove(1, item)}
-                                    onClick={() => moveTask(1, item)}
-                                >
-                                    <FontAwesomeIcon icon={faForward} className="card__button-icon" />
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-            </ul>
+            <ul className="column__list">{renderTasks()}</ul>
         </div>
     );
 };
---
+++ src/components/Task.js
@@ -0,0 +1,85 @@
+﻿import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForward, faBackward, faTrash } from '@fortawesome/free-solid-svg-icons';
+import BoardContext from '../context/context';
+
+const Task = (props) => {
+    const { tasks, setTasks, columns } = useContext(BoardContext);
+    const { data } = props;
+    const { taskName, taskDescription, taskOwner, taskDeadline, id, idColumn } = data;
+
+    const couldTaskMove = (value) => {
+        const newColumn = columns.find((column) => column.id === idColumn + value);
+        if (newColumn) {
+            const tasksInColumn = tasks.filter((task) => task.idColumn === newColumn.id);
+            if (tasksInColumn.length < newColumn.limit) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    const moveTask = (value, taskId) => {
+        if (couldTaskMove(value)) {
+            const movedTask = tasks.findIndex((task) => task.id === taskId);
+
+            const newState = tasks.map((task, index) => {
+                if (index === movedTask) {
+                    return { ...task, idColumn: task.idColumn + value };
+                }
+                return task;
+            });
+
+            setTasks(newState);
+        }
+    };
+
+    const removeTask = (taskId) => {
+        const tasksAfterRemove = tasks.filter((task) => task.id !== taskId);
+        setTasks(tasksAfterRemove);
+    };
+
+    return (
+        <li className="card">
+            <h3 className="card__title">{taskName}</h3>
+            <p className="card__description">{taskDescription}</p>
+            <p className="card__owner">{taskOwner}</p>
+            <p className="card__deadline">{taskDeadline}</p>
+            <div className="card__buttons-box">
+                <button
+                    className="card__button"
+                    type="button"
+                    disabled={!couldTaskMove(-1)}
+                    onClick={() => moveTask(-1, id)}
+                >
+                    <FontAwesomeIcon icon={faBackward} className="card__button-icon" />
+                </button>
+                <button className="card__button" type="button" onClick={() => removeTask(id)}>
+                    <FontAwesomeIcon icon={faTrash} className="card__button-icon" />
+                </button>
+                <button
+                    className="card__button"
+                    type="button"
+                    disabled={!couldTaskMove(1)}
+                    onClick={() => moveTask(1, id)}
+                >
+                    <FontAwesomeIcon icon={faForward} className="card__button-icon" />
+                </button>
+            </div>
+        </li>
+    );
+};
+
+export default Task;
+
+Task.propTypes = {
+    data: PropTypes.shape({
+        taskName: PropTypes.string.isRequired,
+        taskDescription: PropTypes.string.isRequired,
+        taskOwner: PropTypes.string.isRequired,
+        taskDeadline: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        idColumn: PropTypes.number.isRequired,
+    }).isRequired,
+};
