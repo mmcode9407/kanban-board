6dde852927622d63ba31572b9d358fa04de7e3e2
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-04-14 16:30:44 +0200

    use custom hook for set tasks to localStorage and for get items from localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,38 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Form from './Form';
 import Board from './Board';
 import BoardContext from '../context/context';
 import { defaultColumns, defaultTasks } from '../defaultData';
+import useStorage from '../hook';
 
 const App = () => {
-    const [tasks, setTasks] = useState(defaultTasks);
+    const [tasks, setTasks] = useState([]);
     const [columns, setNewColumns] = useState(defaultColumns);
+    const [setStorageTasks, getStorageTasks] = useStorage('tasks');
 
     const { Provider: BoardProvider } = BoardContext;
 
     const addTask = (task) => {
         setTasks([...tasks, task]);
     };
+
+    useEffect(() => {
+        const storageTasks = getStorageTasks();
+        if (!storageTasks || storageTasks.length === 0) {
+            setTasks(defaultTasks);
+            return;
+        }
+
+        setTasks(storageTasks);
+        // eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
+        setStorageTasks(tasks);
+        // eslint-disable-next-line
+    }, [tasks]);
 
     return (
         <div className="wrapper app">
