635e6b73c01fdf31c28b77ca937f859fdc22e6f8
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-04-14 16:29:41 +0200

    create hook for get and set items to localStorage

---
+++ src/hook.js
@@ -0,0 +1,12 @@
+﻿/* eslint-disable no-undef */
+const useStorage = (name) => {
+    const setItem = (data) => {
+        localStorage.setItem(name, JSON.stringify(data));
+    };
+
+    const getItem = () => JSON.parse(localStorage.getItem(name));
+
+    return [setItem, getItem];
+};
+
+export default useStorage;
