2e632dc4a2b117d8d4fc3e007063ad140d6d0a1e
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-04-14 16:17:30 +0200

    create `default data`

---
+++ src/defaultData.js
@@ -0,0 +1,73 @@
+﻿export const defaultColumns = [
+    { id: 1, name: 'Oczekujące', limit: 4 },
+    { id: 2, name: 'W trakcie', limit: 3 },
+    { id: 3, name: 'Testowanie', limit: 2 },
+    { id: 4, name: 'Zrobione', limit: 4 },
+];
+
+export const defaultTasks = [
+    {
+        id: 1,
+        idColumn: 1,
+        taskName: 'Task1',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+    {
+        id: 2,
+        idColumn: 1,
+        taskName: 'Task2',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+    {
+        id: 3,
+        idColumn: 1,
+        taskName: 'Task3',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+    {
+        id: 4,
+        idColumn: 1,
+        taskName: 'Task4',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+    {
+        id: 5,
+        idColumn: 2,
+        taskName: 'Task5',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+    {
+        id: 6,
+        idColumn: 3,
+        taskName: 'Task6',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+    {
+        id: 7,
+        idColumn: 4,
+        taskName: 'Task7',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+    {
+        id: 8,
+        idColumn: 4,
+        taskName: 'Task8',
+        taskOwner: 'Anna',
+        taskDescription: 'Lorem ipsum',
+        taskDeadline: '2023-04-30 10:00',
+    },
+];
